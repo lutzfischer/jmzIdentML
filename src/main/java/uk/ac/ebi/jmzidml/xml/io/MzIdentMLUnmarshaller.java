b667df24e62440a44cf5f0bd7d24f81a4b205818
Lutz Fischer <lutz.fischer@tu-berlin.de>
2025-06-23 15:02:33 +0200

    CodeRabit suggestion

--- src/main/java/uk/ac/ebi/jmzidml/xml/io/MzIdentMLUnmarshaller.java
+++ src/main/java/uk/ac/ebi/jmzidml/xml/io/MzIdentMLUnmarshaller.java
@@ -81,9 +81,9 @@ public class MzIdentMLUnmarshaller {
         this.cache = null;
         if (this.getMzIdentMLVersion().startsWith("1.1")){
             this.mzIdentVersion = MzIdentMLVersion.Version_1_1;
-        } else if (this.mzidVersion.startsWith("1.2")){
+        } else if (this.getMzIdentMLVersion().startsWith("1.2")){
             this.mzIdentVersion = MzIdentMLVersion.Version_1_2;
-        } else if (this.mzidVersion.startsWith("1.3")){
+        } else if (this.getMzIdentMLVersion().startsWith("1.3")){
             this.mzIdentVersion = MzIdentMLVersion.Version_1_3;
         } else {
             throw new IllegalStateException("The mzIdentML file version is not recognized!");
